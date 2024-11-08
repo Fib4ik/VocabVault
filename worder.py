73c542065dc4a05bf5626abcefaf0ab2cb0cca46
scanet <channelfireblock@gmail.com>
2024-11-08 17:13:47 +0200

    Added methods for updating repeats

--- worder.py
+++ worder.py
@@ -1,37 +1,55 @@
 from collections.abc import Iterable
+from typing import Optional
 import sqlite3
 
 
+# TODO write docs
 class WordAcessor:
-    def __init__(self, db_name: str, create_table: bool = True):
+    def __init__(self, db_name: str) -> None:
         self.con = sqlite3.connect(db_name)
         self.cur = self.con.cursor()
-        if create_table:
-            self.create()
+        self.create()
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.con.close()
-        self.cur = None
 
-    def create(self, table_name: str = "word_table"):
-        query = f"CREATE TABLE IF NOT EXISTS {table_name} \
-        (word TEXT(32), translation TEXT(32), repeats INTEGER, \
-        UNIQUE (word), UNIQUE(translation));"
+    def __len__(self) -> int:
+        return self.count()
+
+    def create(self, table_name: str = "word_table") -> None:
+        query = (f"CREATE TABLE IF NOT EXISTS {table_name} "
+                 "(word TEXT(32), translation TEXT(32), repeats INTEGER, "
+                 "UNIQUE (word), UNIQUE(translation));")
         self.table_name = table_name
         self.cur.execute(query)
 
-    def insert(self,
-               words: Iterable,
-               flush: bool = True) -> bool:
+    def insert(self, words: Iterable) -> None:
         if not isinstance(words, Iterable):
-            return False
-        self.cur.executemany(f"INSERT OR IGNORE INTO \
-                {self.table_name} VALUES \
-                (?, ?, ?)", words)
-        if flush:
-            self.con.commit()
-        return True
+            raise TypeError(f'can only insert iterable, not {type(words).__name__}')
+        self.cur.executemany("INSERT OR IGNORE INTO "
+                             f"{self.table_name} VALUES "
+                             "(?, ?, ?)", words)
+        self.con.commit()
 
     def getall(self) -> Iterable:
-        return self.cur.execute(f"SELECT * FROM {self.table_name} \
-                ORDER BY repeats")
+        return self.cur.execute(
+                f"SELECT * FROM {self.table_name} ORDER BY repeats")
+
+    def count(self) -> int:
+        return tuple(
+                self.cur.execute(f"SELECT COUNT(*) FROM {self.table_name}")
+                )[0][0]
+
+    def increment(self, words: Iterable) -> None:
+        for word in words:
+            self.cur.execute(
+                    f"UPDATE {self.table_name} SET repeats = repeats + 1"
+                    " WHERE word = ?", (word,))
+            self.con.commit()
+
+    def reset(self, words: Iterable) -> None:
+        for word in words:
+            self.cur.execute(
+                    f"UPDATE {self.table_name} SET repeats = 0"
+                    " WHERE word = ?", (word,))
+            self.con.commit()
